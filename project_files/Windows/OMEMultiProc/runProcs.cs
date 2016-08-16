bf5d5c31cbe58a1fe718b1d64e09066d635be5b7
vladis466 <predovic900@gmail.com>
2016-08-16 12:34:01 -0700

    finished text to object parsing, implementing parameter extraction and formatting

--- project_files/Windows/OMEMultiProc/runProcs.cs
+++ project_files/Windows/OMEMultiProc/runProcs.cs
@@ -68,8 +68,9 @@ namespace MultiProc
         //plist -> list of all variables and their possible range of setting
         public class modelProfile
         {
-            string base_name;
+            public string base_name;
             public List<paramLRP> plist = new List<paramLRP>();
+            String[] temp_params;
             //Dictionary<key,val>;
 
             public void fill_profile(string path)
@@ -84,20 +85,28 @@ namespace MultiProc
                             if ( line[0] != '#')
                             {
                                 if (line.Substring(0, 2) == "**")
-                                    base_name = line.Replace("*","");
+                                {
+                                    base_name = line.Replace("*", "");
+                                    Console.WriteLine("Loading base profile: {0}", base_name);
+                                }
+                                else
+                                {
+                                    temp_params = line.Replace("\t", String.Empty).Split(',');
+                                    paramLRP new_entry = new paramLRP(temp_params);
+                                    plist.Add(new_entry);
+                                }
                             }
                         }
-                        Console.WriteLine(base_name);
-                        Console.WriteLine("##############End of File");
-
-
-                        Console.WriteLine(path);
-
+                        Console.WriteLine("Load Complete");
                     }
                 }
-                catch
+                catch(Exception e)
                 {
-
+                    Console.WriteLine(e.GetType().FullName);
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Press any button to exit, program will now terminate");
+                    Console.ReadLine();
+                    Environment.Exit(1);
                 }
                 
             }
@@ -106,21 +115,220 @@ namespace MultiProc
         //LRP model parameter ranges
         public class paramLRP
         {
-            string ome_name { get; set; }
-            string descriptor { get; set; }
-            float default_val { get; set; }
-            float min { get; set; }
-            float max { get; set; }
-            float intrvl { get; set; }
-            int iter { get; set; }
+            //Structure of the profiles used is as follows
+            public paramLRP(string[] prams)
+            {
+                try
+                {
+                    ome_name = prams[0];
+                    descriptor = prams[1];
+                    default_val = float.Parse(prams[2]);
+                    min = float.Parse(prams[3]);
+                    max = float.Parse(prams[4]);
+                    intrvl = float.Parse(prams[5]);
+                    iter = int.Parse(prams[6]);
+                    if (iter == 0) { iter = 1; }
+                }
+                catch(IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Index was out of range, check the commas for variable {0}", ome_name );
+                    Console.WriteLine("\n\n Program will now exit, press any button");
+                    Console.ReadLine();
+                    Environment.Exit(1);
+                }
+                catch(InvalidCastException)
+                {
+                    Console.WriteLine("Invalid cast, check the fields in the profile for variable {0}", ome_name);
+                    Console.WriteLine("\n\n Program will now exit, press any button");
+                    Console.ReadLine();
+                    Environment.Exit(1);
+                }
+
+            }
+
+            public string ome_name { get; set; }
+            public string descriptor { get; set; }
+            public float default_val { get; set; }
+            public float min { get; set; }
+            public float max { get; set; }
+            public float intrvl { get; set; }
+            public int iter { get; set; }
 
         }
 
+        
+        
+        
+        
+        
+        
+        
+        //Calculate all combinations, fill in output file names and input string to engine accordingly
+        public static void nameCreator(modelProfile model_data, string[] build_input, string[] build_output)
+        {
+            List<paramLRP> pd = model_data.plist;
+
+            var combinations = from a in pd[0].min.ToString()
+                               from b in pd[1].min.ToString()
+                               from c in pd[2].min.ToString()
+                               orderby a, b, c
+                               select new List<int> { a, b, c };
+
+            var x = combinations.ToList();
  
+
+        }
+
+        //The engine arguments and each base model along with its parameters are passed in
+        public static void LaunchOMEMultiProc(string eng_args, modelProfile model)
+        {
+            string startTime = DateTime.Now.ToString("h:mm:ss tt");
+            Console.WriteLine("Time started: {0}\r\n", startTime);
+            Queue model_queue = new Queue();
+            short proc_cntr = 0;
+            int file_count = 1;
+
+            //First get the amount of combinations to get the file count
+            foreach(paramLRP var in model.plist )
+                file_count *= var.iter;
+
+            //List of models to be ran with complete path
+            //Add this list to Queue
+            string exec_path=AppDomain.CurrentDomain.BaseDirectory;
+            string test_path = Path.GetFullPath(Path.Combine(exec_path, @"..\..\OMEMultiProc\items\compiled_tests"));
+            string[] file_name_arr = Directory.GetFiles(test_path, "*.omec");
+            string[] out_files = new string[file_count];
+            string[] input_params = new string[file_count];
+
+
+            //Call a function to fill all the input/output names. 
+            nameCreator(model, input_params, out_files);
+
+
+
+                //CHANGE AMT OF FILES HERE
+                for (int j = 0; j < file_name_arr.Length * 1024; j++)
+                {
+                    for (int i = 0; i < file_name_arr.Length; i++)
+                    {
+                        model_queue.Enqueue(file_name_arr[i]);
+                    }
+                }
+
+            Console.WriteLine("Finished processing files, starting Engines...");
+            /*
+            while (model_queue.Count > 0)
+            {   
+                
+                //build argument list 
+                Guid g = Guid.NewGuid();
+                string input_path = (string)model_queue.Dequeue();
+                int idx = input_path.LastIndexOf('\\');
+                int len = input_path.LastIndexOf('.') - idx;              
+                string out_file = input_path.Substring(idx + 1, len-1);
+                string out_path = input_path.Substring(0, idx) + "\\results\\" + out_file + g + ".csv";
+               
+               
+                //Prepare a process to start up OMEengine with the proper filepaths and flags
+                Process proc_engine = new Process();
+                proc_engine.StartInfo.FileName = ".\\OMEEngine.exe"; ;
+                //Console.WriteLine(eng_args + " -c\"" + out_path + "\" \"" + input_path + "\"");
+                //Console.ReadLine();
+                proc_engine.StartInfo.Arguments = eng_args + " -c\"" + out_path + "\" \"" + input_path + "\"";
+                proc_engine.EnableRaisingEvents = true;
+                proc_engine.StartInfo.CreateNoWindow = true;
+                proc_engine.StartInfo.UseShellExecute = false;
+                //proc_engine.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+
+                //Manage the amount of processes running at once to maximize performace
+                while (proc_cntr > Environment.ProcessorCount)
+               {
+                   System.Threading.Thread.Sleep(500);
+                   //Console.WriteLine("Waiting for process to exit....Sleeping for 2 seconds");
+               }
+
+                //listener for each process, signals when they exit
+                proc_engine.Exited += (sender, EventArgs) =>
+                    {
+                        proc_cntr -= 1;
+                        //Console.WriteLine("Current count at process exit: {0}\r\n", proc_cntr);
+
+                        if (proc_cntr == 0)
+                        {
+                            Console.WriteLine("Time started: {0}\r\n", startTime);
+                            Console.WriteLine("Time ended: {0}\r\n", DateTime.Now.ToString("h:mm:ss tt"));
+                            Console.WriteLine("Exit time:    {0}\r\n", proc_engine.ExitTime);
+                        }
+
+
+                        if (proc_engine.ExitCode != 0)
+                            Console.Write("Error occured");
+
+                        //Source:
+                        //https://msdn.microsoft.com/en-us/library/system.diagnostics.process.enableraisingevents(v=vs.110).aspx
+
+                    };
+
+                //Start up the given subprocess consisting of a call to OMEENGINE
+                proc_engine.Start();
+                //proc_engine.WaitForExit();
+                proc_cntr += 1;
+                //Console.WriteLine("Current count at process start: {0}\r\n", proc_cntr);
+                  
+            }*/
+
+            Console.WriteLine("Finished starting last set of processes. Please wait for these to complete, then press any button to exit");
+            Console.WriteLine("Time started: {0}\r\n", startTime);
+            Console.WriteLine("Time ended: {0}\r\n", DateTime.Now.ToString("h:mm:ss tt"));
+
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.Write(Environment.NewLine + Environment.NewLine);
+            Console.WriteLine("Enter any flags separated only be spaces: ");
+
+            //Run quietly to improve performance
+            //Hardcode for testing purposes.
+            string engine_args = " -q";
+
+            string exec_path=AppDomain.CurrentDomain.BaseDirectory;
+            string profile_dir = Path.GetFullPath(Path.Combine(exec_path, @"..\..\OMEMultiProc\items\profiles"));
+
+            profileCollection model_lst = new profileCollection(profile_dir);
+            //model_lst.profile_prints.ForEach(item => Console.WriteLine(item.base_name));
+
+            //Iterate through each profile, launching the program
+            for (int i = 0; i < model_lst.profile_prints.Count; i++)
+                LaunchOMEMultiProc(engine_args, model_lst.profile_prints[i]);
     
 
+            Console.ReadLine();
+        }
+    }
+}
 
-        public static void LaunchOMEMultiProc(string eng_args)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*    OLD VERSION FOR TESTING, MAYBE REIMPLEMENT AS TESTING/SINGULAR OPTION
+      public static void LaunchOMEMultiProc(string eng_args)
         {
             string startTime = DateTime.Now.ToString("h:mm:ss tt");
             Console.WriteLine("Time started: {0}\r\n", startTime);
@@ -144,7 +352,7 @@ namespace MultiProc
             }
 
 
-
+            
             while (model_queue.Count > 0)
             {   
                 
@@ -205,33 +413,10 @@ namespace MultiProc
                 //Console.WriteLine("Current count at process start: {0}\r\n", proc_cntr);
                   
             }
+
             Console.WriteLine("Finished starting last set of processes. Please wait for these to complete, then press any button to exit");
             Console.WriteLine("Time started: {0}\r\n", startTime);
             Console.WriteLine("Time ended: {0}\r\n", DateTime.Now.ToString("h:mm:ss tt"));
             Console.ReadLine(); 
 
-        }
-
-
-
-
-        static void Main(string[] args)
-        {
-            Console.Write(Environment.NewLine + Environment.NewLine);
-            Console.WriteLine("Enter any flags separated only be spaces: ");
-
-            //Run quietly to improve performance
-            //Hardcode for testing purposes.
-            string engine_args = " -q";
-
-            string exec_path=AppDomain.CurrentDomain.BaseDirectory;
-            string profile_dir = Path.GetFullPath(Path.Combine(exec_path, @"..\..\OMEMultiProc\items\profiles"));
-
-            profileCollection modelList = new profileCollection(profile_dir);
-
-
-            //LaunchOMEMultiProc(engine_args);
-            Console.ReadLine();
-        }
-    }
-}
+        }*/
